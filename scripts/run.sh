52b83081f57844054d25973821b682a84acd1176
Stephen Mayhew <swm.mayhew@gmail.com>
2018-03-27 12:07:22 -0400

    updated run.sh

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
-
+if [ ! -f config/users.txt ]; then
+    echo "config/users.txt not found. Try running: python scripts/quickstart.py"
+    exit
+fi
 
 
 mvn spring-boot:run -Drun.addResources=true
