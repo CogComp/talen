419baa86a5072195ed37ad9427295611b23e4a18
Stephen Mayhew <swm.mayhew@gmail.com>
2018-05-08 10:16:14 -0400

    Migrate to spring boot 2, definitely broken right now

--- src/main/java/io/github/mayhewsw/Application.java
+++ src/main/java/io/github/mayhewsw/Application.java
@@ -2,14 +2,9 @@ package io.github.mayhewsw;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
-
-import javax.servlet.http.HttpSessionListener;
 
 @SpringBootApplication
-public class Application extends WebMvcConfigurerAdapter {
+public class Application {
 
     public static void main(String[] args) {
 
