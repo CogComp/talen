40fa7d6ef72b4208ead4b035379f1e9c7667588b
Stephen Mayhew <swm.mayhew@gmail.com>
2018-05-08 22:08:55 -0400

    Fixed bugs that would give security error when empty line was in users.txt

--- src/main/java/io/github/mayhewsw/WebSecurityConfig.java
+++ src/main/java/io/github/mayhewsw/WebSecurityConfig.java
@@ -3,12 +3,18 @@ package io.github.mayhewsw;
 
 import edu.illinois.cs.cogcomp.core.io.LineIO;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
+import java.io.FileNotFoundException;
 import java.util.ArrayList;
 
 @Configuration
@@ -30,15 +36,36 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
         http.csrf().disable();
     }
 
-    @Autowired
-    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+    @Bean
+    @Override
+    public UserDetailsService userDetailsService() {
 
-        ArrayList<String> users = LineIO.read("config/users.txt");
+        InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
 
-        for(String up : users){
-            String[] userpass = up.split("\t");
-            auth.inMemoryAuthentication()
-                    .withUser(userpass[0]).password(userpass[1]).roles("USER");
+        try {
+            ArrayList<String> users = LineIO.read("config/users.txt");
+
+            for(String up : users){
+                String[] userpass = up.split("\\s+");
+                if(userpass.length == 2) {
+                    UserDetails user = User.withDefaultPasswordEncoder()
+                            .username(userpass[0])
+                            .password(userpass[1])
+                            .roles("USER")
+                            .build();
+                    manager.createUser(user);
+                }
+            }
+
+        } catch (FileNotFoundException e) {
+            System.err.println("config/users.txt file not found. Using default user/password instead.");
+            UserDetails user = User.withDefaultPasswordEncoder()
+                    .username("user")
+                    .password("password")
+                    .roles("USER")
+                    .build();
+            manager.createUser(user);
         }
+        return manager;
     }
 }
