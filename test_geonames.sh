dc2aa2c4ddc1b99342cb67646d882c3df0b59df7
Shyam Upadhyay <shyamupa@gmail.com>
2018-07-03 15:59:30 -0400

    run script for testing

---
+++ test_geonames.sh
@@ -0,0 +1,10 @@
+CP="./config/:./target/classes/:./target/dependency/*"
+
+MEMORY="-Xmx30g"
+OPTIONS="$MEMORY -Xss40m -ea -cp $CP"
+PACKAGE_PREFIX="edu.illinois.cs.cogcomp"
+
+#MAIN="$PACKAGE_PREFIX.lorelei.AddTab"
+MAIN="$PACKAGE_PREFIX.lorelei.kb.MapDBCandGen"
+# time nice java $OPTIONS $MAIN $*
+time mvn exec:java -Dexec.mainClass=$MAIN -Dexec.args="$*"
