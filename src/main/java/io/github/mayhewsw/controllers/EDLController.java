bbf7469bb31fa9f3160dcad1d602daa1716b0dd8
psanna77 <poudyal@seas.upenn.edu>
2018-07-27 12:24:45 -0400

    File converter now expects text annotation

--- src/main/java/io/github/mayhewsw/controllers/EDLController.java
+++ src/main/java/io/github/mayhewsw/controllers/EDLController.java
@@ -262,9 +262,9 @@ public class EDLController {
             labels.add(sl[0]);
             csslines.add("." + sl[0] + "{ background-color: " + sl[1] + "; }");
         }
-        if(!labels.contains("MISC")) {
-            labels.add("MISC");
-            csslines.add(".MISC{ background-color: lightgray; }" );
+        if(!labels.contains("MENTION")) {
+            labels.add("MENTION");
+            csslines.add(".MENTION{ background-color: lightgray; }" );
         }
         logger.debug("using labels: " + labels.toString());
         LineIO.write("src/main/resources/static/css/labels.css", csslines);
--- src/main/java/io/github/mayhewsw/utils/FileConverter.java
+++ src/main/java/io/github/mayhewsw/utils/FileConverter.java
@@ -1,31 +1,25 @@
 package io.github.mayhewsw.utils;
 
-import edu.illinois.cs.cogcomp.annotation.TextAnnotationBuilder;
-import edu.illinois.cs.cogcomp.core.datastructures.IntPair;
 import edu.illinois.cs.cogcomp.core.datastructures.ViewNames;
 import edu.illinois.cs.cogcomp.core.datastructures.textannotation.Constituent;
 import edu.illinois.cs.cogcomp.core.datastructures.textannotation.SpanLabelView;
 import edu.illinois.cs.cogcomp.core.datastructures.textannotation.TextAnnotation;
 import edu.illinois.cs.cogcomp.core.datastructures.textannotation.View;
-import edu.illinois.cs.cogcomp.nlp.tokenizer.IllinoisTokenizer;
-import edu.illinois.cs.cogcomp.nlp.utility.TokenizerTextAnnotationBuilder;
-import org.apache.commons.lang.StringUtils;
-import org.apache.tomcat.util.bcel.Const;
+import edu.illinois.cs.cogcomp.core.utilities.SerializationHelper;
 
-import java.io.*;
-import java.nio.file.Files;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
 import java.util.*;
-import java.nio.file.Paths;
 
 public class FileConverter{
-    public static List<TextAnnotation> fromTabSeparated(String folderurl) throws IOException {
+    public static List<TextAnnotation> fromTabSeparated(String folderurl) throws Exception {
         return fromTabSeparated(folderurl, folderurl);
     }
 
-    public static List<TextAnnotation> fromTabSeparated(String corpusfolderurl, String tabfolderurl) throws IOException {
+    public static List<TextAnnotation> fromTabSeparated(String corpusfolderurl, String tabfolderurl) throws Exception {
         File[] tabFiles = fileFinder(tabfolderurl, ".tab");
-        TextAnnotationBuilder taBuilder = new TokenizerTextAnnotationBuilder(new IllinoisTokenizer());
-        HashMap<String, TextAnnotation> docsSeen = new HashMap<>();
+        Map<String, TextAnnotation> tasSeen = new HashMap<>();
 
         List<TextAnnotation> ret = new ArrayList<>();
         for(File tabfile : tabFiles){
@@ -34,69 +28,55 @@ public class FileConverter{
 
             while((line = br.readLine()) != null){
                 String[] entry = line.split("\t");
-                String docName = entry[0];
                 String startTok = entry[1];
                 String endTok = entry[2];
-                String[] candidates;
-                String token = null;
-                if(!(StringUtils.isNumeric(startTok) && StringUtils.isNumeric(endTok))){
-                    candidates = Arrays.copyOfRange(entry, 2, entry.length);
-                    token = entry[1];
+                String[] candidates = Arrays.copyOfRange(entry, 3, entry.length);
+
+                View candgen, ner_conll;
+                TextAnnotation ta;
+
+                if(tasSeen.keySet().contains(entry[0].trim())){
+                    ta = tasSeen.get(entry[0].trim());
                 } else {
-                    candidates = Arrays.copyOfRange(entry, 3, entry.length);
+                    ta = SerializationHelper.deserializeTextAnnotationFromFile(corpusfolderurl + entry[0]);
                 }
 
-                TextAnnotation ta;
-                View candgen, ner_conll;
-                if(docsSeen.keySet().contains(docName.trim())){
-                    ta = docsSeen.get(docName.trim());
+                if(ta.hasView("CANDGEN")){
                     candgen = ta.getView("CANDGEN");
+                } else {
+                    candgen = new SpanLabelView("CANDGEN", ta);
+                }
+                if(ta.hasView(ViewNames.NER_CONLL)) {
                     ner_conll = ta.getView(ViewNames.NER_CONLL);
                 } else {
-                    ta = taBuilder.createTextAnnotation(
-                            corpusfolderurl,
-                            docName,
-                            String.join(
-                                    " ",
-                                    Files.readAllLines(
-                                            Paths.get(
-                                                    corpusfolderurl + docName
-                                            )
-                                    )
-                            )
-                    );
-                    candgen = new SpanLabelView("CANDGEN", ta);
                     ner_conll = new SpanLabelView(ViewNames.NER_CONLL, ta);
                 }
 
                 Map<String, Double> lts = new TreeMap<>();
-
                 for(String cand: candidates){
                     lts.put("-1|" + cand, 0.0);
                 }
 
-                if(StringUtils.isNumeric(startTok) && StringUtils.isNumeric(endTok)) {
-                    Constituent newc = new Constituent(lts, "CANDGEN", ta, Integer.parseInt(startTok), Integer.parseInt(endTok));
-                    Constituent conll_c = new Constituent("MISC", ViewNames.NER_CONLL, ta, Integer.parseInt(startTok), Integer.parseInt(endTok));
-                    candgen.addConstituent(newc);
-                    ner_conll.addConstituent(conll_c);
-                } else {
-                    List<IntPair> spans = ta.getSpansMatching(token.trim());
+                Constituent newc, conll_c;
+                if(!candgen.getConstituentsCoveringSpan(Integer.parseInt(startTok), Integer.parseInt(endTok)).isEmpty()){
+                    Constituent oldc = candgen.getConstituentsCoveringSpan(Integer.parseInt(startTok), Integer.parseInt(endTok)).get(0);
+                    Map<String, Double> old_lts = oldc.getLabelsToScores();
+                    old_lts.forEach(lts::putIfAbsent);
+                }
+                newc = new Constituent(lts, "CANDGEN", ta, Integer.parseInt(startTok), Integer.parseInt(endTok));
+                candgen.addConstituent(newc);
 
-                    for(IntPair span : spans){
-                        Constituent newc = new Constituent(lts, "CANDGEN", ta, span.getFirst(), span.getSecond());
-                        Constituent conll_c = new Constituent("MISC", ViewNames.NER_CONLL, ta, span.getFirst(), span.getSecond());
-                        candgen.addConstituent(newc);
-                        ner_conll.addConstituent(conll_c);
-                    }
+                if(ner_conll.getConstituentsCoveringSpan(Integer.parseInt(startTok), Integer.parseInt(endTok)).isEmpty()){
+                    conll_c = new Constituent("MENTION", ViewNames.NER_CONLL, ta, Integer.parseInt(startTok), Integer.parseInt(endTok));
+                    ner_conll.addConstituent(conll_c);
                 }
 
-                ta.addView("CANDGEN", candgen);
-                ta.addView(ViewNames.NER_CONLL, ner_conll);
+                if(!ta.hasView("CANDGEN")){ta.addView("CANDGEN", candgen);}
+                if(!ta.hasView(ViewNames.NER_CONLL)){ta.addView(ViewNames.NER_CONLL, ner_conll);}
                 ret.add(ta);
 
-                if(!docsSeen.keySet().contains(docName)){
-                    docsSeen.put(docName.trim(), ta);
+                if(!tasSeen.keySet().contains(entry[0].trim())){
+                    tasSeen.put(entry[0].trim(), ta);
                 }
             }
         }
