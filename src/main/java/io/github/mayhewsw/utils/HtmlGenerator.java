02a64d12108ffcd7bb2b5a469833762d0ddae27f
ttasya <tanya.tspe@gmail.com>
2019-07-15 12:13:57 -0400

    added showgoogle (eigth try)

--- src/main/java/io/github/mayhewsw/utils/HtmlGenerator.java
+++ src/main/java/io/github/mayhewsw/utils/HtmlGenerator.java
@@ -161,6 +161,7 @@ public class HtmlGenerator {
      */
     public static String getHTMLfromTA(TextAnnotation ta, IntPair span, String id, String query, Dictionary dict, boolean showdefs, boolean showroman, boolean allowcopy, boolean showgoogle) {
 
+        System.out.println("Inside getHTMLfromTA method");
         IntPair sentspan = span;
 
         // required to have one view or another...
